4a825271f16942da85791095cffd6bf380c2333c
俞佳兴 <albertyu@ys-macbook-pro.local>
2019-01-08 23:29:01 +0800

    Initial Commit

---
+++ leanDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  leanDemo
+//
+//  Created by 俞佳兴 on 2018/12/17.
+//  Copyright © 2018 Albert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
+
